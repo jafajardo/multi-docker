c0003df8d454e62cdf4b6c05c3853bf199a88edb
Joseph Fajardo <jafajardo@gmail.com>
2021-04-07 15:20:41 +1000

    Made changes to app

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ function App() {
 			<div className="App">
 				<header className="App-header">
 					<img src={logo} className="App-logo" alt="logo" />
+					<h1>Fib Calculator</h1>
 				</header>
 				<div>
 					<Route exact path="/" component={Fib} />

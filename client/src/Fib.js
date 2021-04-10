7aa45924c3fe0126d7274a6d9c3590e61cc6e3f6
Joseph Fajardo <jafajardo@gmail.com>
2021-04-10 17:31:21 +1000

    Update Fib to return early

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -35,7 +35,7 @@ class Fib extends Component {
 	};
 
 	renderSeenIndexes() {
-		if (this.state.seenIndexes.length <= 0) return null;
+		if (!this.state.seenIndexes.length) return null;
 		return this.state.seenIndexes.map(({ number }) => number).join(', ');
 	}
 
